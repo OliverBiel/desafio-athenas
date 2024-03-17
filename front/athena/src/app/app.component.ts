1ddd4b20d0ecabfd685016e0c1b41fe38b4a066c
oliverbiel <gmartins.oliver@gmail.com>
2024-03-17 14:56:46 -0300

    feat: q param para busca

--- front/athena/src/app/app.component.ts
+++ front/athena/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Person } from './interfaces/person';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,21 @@ import { Person } from './interfaces/person';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor (private router: Router,
+    private route: ActivatedRoute) {}
+
   title = 'athena';
 
   result: Person[] = [];
   showModal: boolean = false;
+  search: string = '';
   
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.search = params['search'];
+    });
+  }
+
   setResult(data: Person[]) {
     this.result = data;
   }
--- front/athena/src/app/components/search/search.component.ts
+++ front/athena/src/app/components/search/search.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Output, EventEmitter } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { Person } from 'src/app/interfaces/person';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -13,21 +14,41 @@ import { Person } from 'src/app/interfaces/person';
 export class SearchComponent {
 
   constructor(
-    private apiService: ApiService
+    private apiService: ApiService,
+    private router: Router,
+    private route: ActivatedRoute
   ) { }
-  
+  @Input() search: string|undefined = undefined;
   @Output() result = new EventEmitter<Person[]>();
 
+  
   searchForm = new FormGroup({
     search: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(100)])
   });
-
+  
   isValid: boolean = false;
-
+  
   ngOnInit(): void {
+    if (this.search) {
+      this.searchForm.setValue({ search: this.search });
+      this.onSubmit();
+    }
+
     this.searchForm.valueChanges.subscribe((value) => {
       this.isValid = this.searchForm.valid;
+      this.router.navigate([], {
+        queryParams: { search: value.search },
+        queryParamsHandling: 'merge'
+      });
     });
+
+  }
+
+  ngOnChanges(): void {
+    if (this.search) {
+      this.searchForm.setValue({ search: this.search });
+      this.onSubmit();
+    }
   }
 
   onSubmit() {
